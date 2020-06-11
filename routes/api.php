227474ac7eeda501d5bea1f0399666d13ebb51fc
TakehiroTada <taketada.works.gmail.com>
2020-06-11 15:53:20 +0900

    wip

--- routes/api.php
+++ routes/api.php
@@ -13,28 +13,28 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(
-    [],
-    function () {
-        Route::prefix('auth')->group(
-            function () {
-                Route::post('login', 'AuthController@login');
-            }
-        );
-    }
-);
+// Route::group(
+//     [],
+//     function () {
+//         Route::prefix('auth')->group(
+//             function () {
+//                 Route::post('login', 'AuthController@login');
+//             }
+//         );
+//     }
+// );
 
-Route::middleware(['auth:api'])->group(
-    [],
-    function () {
-        Route::resource('users', 'UserController', ['except' => ['create', 'edit']]);
-        Route::prefix('auth')->group(
-            [],
-            function () {
-                Route::post('logout', 'AuthController@logout');
-                Route::post('refresh', 'AuthController@refresh');
-                Route::post('me', 'AuthController@me');
-            }
-        );
-    }
-);
+// Route::middleware(['auth:api'])->group(
+//     [],
+//     function () {
+//         Route::resource('users', 'UserController', ['except' => ['create', 'edit']]);
+//         Route::prefix('auth')->group(
+//             [],
+//             function () {
+//                 Route::post('logout', 'AuthController@logout');
+//                 Route::post('refresh', 'AuthController@refresh');
+//                 Route::post('me', 'AuthController@me');
+//             }
+//         );
+//     }
+// );
