7c64e973a77fa76aa6c33b7b1b941070461d475e
TakehiroTada <taketada.works.gmail.com>
2020-06-12 00:30:20 +0900

    :wrench: route

--- routes/api.php
+++ routes/api.php
@@ -13,23 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(
-    [],
+Route::prefix('auth')->group(
     function () {
-        Route::prefix('auth')->group(
-            function () {
-                Route::post('login', 'AuthController@login');
-            }
-        );
+        Route::post('login', 'AuthController@login');
     }
 );
 
 Route::middleware(['auth:api'])->group(
-    [],
     function () {
         Route::resource('users', 'UserController', ['except' => ['create', 'edit']]);
         Route::prefix('auth')->group(
-            [],
             function () {
                 Route::post('logout', 'AuthController@logout');
                 Route::post('refresh', 'AuthController@refresh');
