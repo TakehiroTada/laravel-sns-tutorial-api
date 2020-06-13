0d4d164b948c4c7549d01797f7fa27be648661a0
TakehiroTada <taketada.works.gmail.com>
2020-06-14 00:07:03 +0900

    fix: userform

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Requests\UserRequest;
 use App\User;
 
 /**
