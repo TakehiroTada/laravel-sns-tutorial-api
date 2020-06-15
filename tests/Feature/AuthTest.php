ea4f97874b8f5b0c2d12d9df97670ad2011878b7
Takehito Tada <taketada.works@gmail.com>
2020-06-15 16:12:26 +0900

    feat: test me

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -6,9 +6,53 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthTest extends TestCase
 {
+    /**
+     * __construct
+     *
+     * @param  mixed $name
+     * @param  mixed $data
+     * @param  mixed $dataName
+     * @return void
+     */
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->createApplication();
+    }
+    /**
+     * testLogin
+     *
+     * @dataProvider loginDataProvider
+     * @param  mixed $expect
+     * @param  mixed $responseCode
+     * @return void
+     */
+    public function testLogin($expect, $responseCode): void
+    {
+        $response = $this->post('/api/auth/login', $expect);
+        $response->assertStatus($responseCode);
+    }
+
+    /**
+     * testMe
+     *
+     * @dataProvider meDataProvider
+     * @param  mixed $token
+     * @param  mixed $expect
+     * @return void
+     */
+    public function testMe($token, $expect): void
+    {
+        $this->withHeader('Authorization', "Bearer {$token}");
+        $response = $this->get('/api/auth/me');
+
+        $response->assertJsonFragment($expect);
+    }
+
     /**
      * A basic feature test example.
      *
@@ -24,21 +68,65 @@ class AuthTest extends TestCase
     }
 
     /**
+     * loginDataProvider
+     *
+     * @return array
+     */
+    public function loginDataProvider(): array
+    {
+        $normalCase = User::find(1)->toArray();
+        $normalCase['password'] = 'password';
+        $abnormalCase = [
+            "email" => "badcase@example.com",
+            "password" => "paaaaaaasword"
+        ];
+
+        return [
+            [$normalCase, 200],
+            [$abnormalCase, 401]
+        ];
+    }
+
+    /**
+     * meDataProvider
+     *
+     * @return array
+     */
+    public function meDataProvider(): array
+    {
+        $user = User::find(1);
+        $token = JWTAuth::fromUser($user);
+        $authlized = [
+            "account_id" => "superuser",
+            "email" => "admin@example.com",
+        ];
+
+        $wrongToken = "hogehogehogehgoehogehogehogehogehogehogehoge";
+        $unAuthlized = [
+            "error" => "Unauthenticated."
+        ];
+
+        return [
+            [$token, $authlized],
+            [$wrongToken, $unAuthlized]
+        ];
+    }
+
+    /**
      * registerDataProvider
      *
      * @return array
      */
     public function registerDataProvider(): array
     {
-        $this->createApplication();
         $normalCase = factory(User::class)->make()->toArray();
         $normalCase['password'] = 'password';
-        $abnormalCase = factory(User::class)->make(['account_id'=> null]);
+        $abnormalCase = factory(User::class)->make(['account_id' => null]);
         $abnormalCase['password'] = 'password';
 
-        return[
+        return [
             [$normalCase, 201],
-            [$abnormalCase->toArray(), 500 ]
+            [$abnormalCase->toArray(), 500]
         ];
     }
 }
